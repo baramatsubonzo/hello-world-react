b9bc388a99691c83a84fede6873289ad6f1a956c
bonzo <40222661+baramatsubonzo@users.noreply.github.com>
2021-02-21 13:34:06 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./HelloWorld";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<HelloWorld />, rootElement);
